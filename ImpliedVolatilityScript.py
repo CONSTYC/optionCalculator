e76ca87383e035ececece0eb98482962216b04e3
Consty <constychen@outlook.com>
2019-02-01 10:53:14 +0800

    minor fix

--- ImpliedVolatilityScript.py
+++ ImpliedVolatilityScript.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
-from bin.PricingF import BSMImpVol, BAWImpVol
+from PricingF import BSMImpVol, BAWImpVol
 import sys
 from datetime import datetime, timedelta
 
@@ -40,7 +40,7 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
 
     def initUI(self):
         FontSize = 10
-        Font_Label = '微软雅黑'
+        Font_Label = 'Palatino Linotype'
 
         self.StatusBar = self.statusBar()
 
@@ -50,53 +50,53 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(ImpliedVol_Center)
 
         Input_Parameter_Text = [
-            '行权价', '市价', '利率',
-            '年天数', '股息率', 'BAW精度'
+            'Stike', 'Price', 'Rate',
+            'days', 'Dividend', 'Precision'
         ]
         self.strike = QtWidgets.QDoubleSpinBox()
         self.strike.setDecimals(2)
         self.strike.setSingleStep(0.01)
         self.strike.setMaximum(999999)
         self.strike.setValue(100)
-        self.strike.setStatusTip('输入行权价格')
+        self.strike.setStatusTip('Input Strike')
 
         self.price = QtWidgets.QDoubleSpinBox()
         self.price.setDecimals(2)
         self.price.setSingleStep(0.01)
         self.price.setMaximum(999999)
         self.price.setValue(100)
-        self.price.setStatusTip('输入当前市价')
+        self.price.setStatusTip('Input current price')
 
         # self.vol = QtWidgets.QLineEdit()
         # self.vol.setText('20')
-        # self.vol.setStatusTip('输入波动率')
+        # self.vol.setStatusTip('Input current volatility')
 
         self.rate = QtWidgets.QDoubleSpinBox()
         self.rate.setDecimals(4)
         self.rate.setSingleStep(0.0001)
         self.rate.setValue(0.03)
-        self.rate.setStatusTip('输入无风险利率')
+        self.rate.setStatusTip('Input risk-free rate')
 
         self.ty = QtWidgets.QSpinBox()
         self.ty.setMaximum(366)
         self.ty.setValue(360)
-        self.ty.setStatusTip('输入一年的天数')
+        self.ty.setStatusTip('Input days per year')
 
         # self.maturity = QtWidgets.QLineEdit()
         # self.maturity.setText('30')
-        # self.maturity.setStatusTip('输入期权期限')
+        # self.maturity.setStatusTip('Input time to maturity')
 
         self.dividend = QtWidgets.QDoubleSpinBox()
         self.dividend.setDecimals(4)
         self.dividend.setSingleStep(0.0001)
         self.dividend.setValue(0.03)
-        self.dividend.setStatusTip('输入股息率')
+        self.dividend.setStatusTip('Input dividend rate')
 
         self.epsilon = QtWidgets.QDoubleSpinBox()
         self.epsilon.setDecimals(7)
         self.epsilon.setValue(0.00001)
         self.epsilon.setSingleStep(0.0000001)
-        self.epsilon.setStatusTip('输入BAW精度')
+        self.epsilon.setStatusTip('Input BAW precision (only for american options)')
 
         Input_Parameter = [
             self.strike, self.price, self.rate,
@@ -129,19 +129,19 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
         self.initialvol.setDecimals(4)
         self.initialvol.setSingleStep(0.0001)
         self.initialvol.setValue(0.5)
-        self.initialvol.setStatusTip('输入波动率初值，一般情况下不用更改')
+        self.initialvol.setStatusTip('Initial guess for volatility')
         self.initialvol.setFixedWidth(140)
         self.initialvol.setFont(QtGui.QFont(Font_Label, FontSize))
         grid.addWidget(self.initialvol, 0, 4)
-        Label = QtWidgets.QLabel('波动率初值')
+        Label = QtWidgets.QLabel('Initial volatility')
         Label.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
         grid.addWidget(Label, 0, 3)
 
         self.callput = QtWidgets.QComboBox()
-        self.callput.addItems(['看涨', '看跌'])
+        self.callput.addItems(['Call', 'Put'])
         self.callput.setFont(QtGui.QFont(Font_Label, FontSize))
         grid.addWidget(self.callput, 2, 4)
-        typeLabel = QtWidgets.QLabel('&o: 期权类型')
+        typeLabel = QtWidgets.QLabel('&o: Option type')
         typeLabel.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
         typeLabel.setBuddy(self.callput)
         grid.addWidget(typeLabel, 2, 3)
@@ -149,7 +149,7 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
         self.maturityDate = QtWidgets.QDateEdit()
         Tdate = datetime.now() + timedelta(days=30)
         self.maturityDate.setDate(QtCore.QDate(Tdate.year, Tdate.month, Tdate.day))
-        self.maturityDate.setStatusTip('按到期日计算隐含波动率')
+        self.maturityDate.setStatusTip('Calculate implied volatolity based on maturity date')
         self.maturityDate.setFixedWidth(140)
         self.maturityDate.setFont(QtGui.QFont(Font_Label, FontSize))
         grid.addWidget(self.maturityDate, 3, 4)
@@ -157,23 +157,23 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
         self.maturity = QtWidgets.QSpinBox()
         self.maturity.setValue(30)
         self.maturity.setMaximum(999999)
-        self.maturity.setStatusTip('按期限计算隐含波动率')
+        self.maturity.setStatusTip('Calculate implied volatolity based on time to maturity')
         self.maturity.setFixedWidth(140)
         self.maturity.setFont(QtGui.QFont(Font_Label, FontSize))
         grid.addWidget(self.maturity, 3, 4)
         self.maturityLabel = QtWidgets.QComboBox()
-        self.maturityLabel.addItems(['期限（天）', '到期日'])
+        self.maturityLabel.addItems(['TTM', 'M Date'])
         self.maturityLabel.currentTextChanged.connect(self.maturityTypeChanged)
         self.maturityLabel.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
         grid.addWidget(self.maturityLabel, 3, 3)
 
-        optpricetypeLabel = QtWidgets.QLabel('输入期权价格')
+        optpricetypeLabel = QtWidgets.QLabel('Option price')
         optpricetypeLabel.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
         grid.addWidget(optpricetypeLabel, 4, 3, 1, 2)
 
         self.optpricetype = QtWidgets.QComboBox()
         self.optpricetype.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
-        self.optpricetype.addItems(['绝对价格', '相对价格'])
+        self.optpricetype.addItems(['Absolute price', 'Relative price'])
         grid.addWidget(self.optpricetype, 5, 3)
 
         self.optprice = QtWidgets.QDoubleSpinBox()
@@ -181,7 +181,7 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
         self.optprice.setDecimals(4)
         self.optprice.setSingleStep(0.0001)
         self.optprice.setMaximum(99999)
-        self.optprice.setStatusTip('输入期权价格')
+        self.optprice.setStatusTip('Input option price')
         self.optprice.setFont(QtGui.QFont(Font_Label, FontSize))
         grid.addWidget(self.optprice, 5, 4)
 
@@ -189,24 +189,24 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
         Label.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
         grid.addWidget(Label, 6, 0, 1, 3)
 
-        Label = QtWidgets.QLabel('BSM隐含波动率：')
+        Label = QtWidgets.QLabel('BSM implied volatility：')
         Label.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
         Label.setAlignment(QtCore.Qt.AlignCenter)
         grid.addWidget(Label, 7, 0, 1, 3)
-        Label = QtWidgets.QLabel('BAW隐含波动率：')
+        Label = QtWidgets.QLabel('BAW implied volatility：')
         Label.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
         Label.setAlignment(QtCore.Qt.AlignCenter)
         grid.addWidget(Label, 8, 0, 1, 3)
 
-        Calculate_Btn = QtWidgets.QPushButton('计算', self)
+        Calculate_Btn = QtWidgets.QPushButton('Compute', self)
         Calculate_Btn.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
-        Calculate_Btn.setStatusTip('计算隐含波动率')
+        Calculate_Btn.setStatusTip('Compute implied volatility')
         Calculate_Btn.clicked.connect(self.ImpVolCalculate)
         grid.addWidget(Calculate_Btn, 9, 0, 1, 3)
 
-        Quit_Btn = QtWidgets.QPushButton('退出', self)
+        Quit_Btn = QtWidgets.QPushButton('Exit', self)
         Quit_Btn.setFont(QtGui.QFont(Font_Label, FontSize, QtGui.QFont.Bold))
-        Quit_Btn.setStatusTip('退出隐含波动率计算器')
+        Quit_Btn.setStatusTip('Close implied volatility calculator')
         Quit_Btn.clicked.connect(self.close)
         grid.addWidget(Quit_Btn, 9, 3, 1, 3)
 
@@ -221,7 +221,7 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
         grid.addWidget(self.BAWImpliedVol_Label, 8, 3, 1, 3)
 
         self.move(300, 300)
-        self.setWindowTitle('隐含波动率计算器')
+        self.setWindowTitle('Implied Volatility Calculator')
 
     def ImpVolCalculate(self):
         try:
@@ -234,9 +234,9 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
             epsilon = self.epsilon.value()
             opt = self.optprice.value()
 
-            if self.optpricetype.currentText() == '相对价格':
+            if self.optpricetype.currentText() == 'Relative price':
                 opt = opt * F
-            if self.maturityLabel.currentText() == '期限（天）':
+            if self.maturityLabel.currentText() == 'TTM':
                 T = float(self.maturity.text()) / ty
             else:
                 mature = str(
@@ -249,12 +249,12 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
                 T = (
                     datetime.strptime(mature, '%Y-%m-%d') - datetime.now() + timedelta(days=1)
                 ).days / ty
-            if self.callput.currentText() == '看涨':
+            if self.callput.currentText() == 'Call':
                 OptionType = 'call'
-            elif self.callput.currentText() == '看跌':
+            elif self.callput.currentText() == 'Put':
                 OptionType = 'put'
         except:
-            self.StatusBar.showMessage('参数输入错误，请检查参数')
+            self.StatusBar.showMessage('Wrong parameters')
 
         # try:
         #     opt = float(self.optprice.text())
@@ -282,7 +282,7 @@ class ImpliedVolatilityWindow(QtWidgets.QMainWindow):
         self.close()
 
     def maturityTypeChanged(self):
-        if self.maturityLabel.currentText() == '期限（天）':
+        if self.maturityLabel.currentText() == 'TTM':
             self.maturity.show()
             self.maturityDate.hide()
         else:
